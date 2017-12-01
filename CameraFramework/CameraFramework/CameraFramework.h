7ce08ce42cb61708fba20497aa0a821dcbf81ef5
Ariel Rodriguez <ariel@volonbolon.me>
2017-12-01 12:21:47 -0300

    Add Xcode project

---
+++ CameraFramework/CameraFramework/CameraFramework.h
@@ -0,0 +1,19 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Ariel Rodriguez on 01/12/2017.
+//  Copyright © 2017 Ariel Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
